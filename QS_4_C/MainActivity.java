52cf956c56faeae98a9bbc9b333184c4c0de0f92
sruthi kalakuntla <48819010+sruthikalakuntla@users.noreply.github.com>
2021-02-06 23:09:56 +0530

    Create MainActivity.java

---
+++ QS_4_C/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a4_c;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtMobile;
+    private EditText txtMessage;
+    private Button btnSms;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtMobile = (EditText) findViewById(R.id.mblTxt);
+        txtMessage = (EditText) findViewById(R.id.msgTxt);
+        btnSms = (Button) findViewById(R.id.btnSend);
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse("smsto:"));
+                    i.setType("vnd.android-dir/mms-sms");
+                    i.putExtra("address", new String(txtMobile.getText().toString()));
+                    i.putExtra("sms_body", txtMessage.getText().toString());
+                    startActivity(Intent.createChooser(i, "Send sms via:"));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "SMS Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
