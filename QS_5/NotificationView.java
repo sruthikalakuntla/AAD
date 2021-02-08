aaa0f5e8ead840b4bae4ff6cb44c8e9febcf68ef
sruthi kalakuntla <48819010+sruthikalakuntla@users.noreply.github.com>
2021-02-09 00:14:45 +0530

    Create NotificationView.java

---
+++ QS_5/NotificationView.java
@@ -0,0 +1,16 @@
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+public class NotificationView extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_view);
+        textView = findViewById(R.id.textView);
+//getting the notification message
+        String message=getIntent().getStringExtra("message");
+        textView.setText(message);
+    }
+}
